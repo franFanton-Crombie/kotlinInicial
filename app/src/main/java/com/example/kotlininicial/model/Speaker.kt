92e3729f083863f505dcf0fa90b6958bb2e9b6ef
franFanton-4R <francisco.fanton@4rsoluciones.com>
2021-08-19 16:05:04 -0300

    se agregaron varias pantallas

---
+++ app/src/main/java/com/example/kotlininicial/model/Speaker.kt
@@ -0,0 +1,11 @@
+package com.example.kotlininicial.model
+
+class Speaker {
+    var name = ""
+    var jobtitle = ""
+    var workplace = ""
+    var biography = ""
+    var twitter = ""
+    var image = ""
+    var category = 0
+}
